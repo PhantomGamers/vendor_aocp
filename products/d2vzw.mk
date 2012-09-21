aa29237aa1f95e649ee69f41f6f380083d73e813
PhantomGamers <phantom@sundergaming.com>
2012-09-20 23:30:21 -0400

    d2vzw: bring ug up to date

--- products/d2vzw.mk
+++ products/d2vzw.mk
@@ -1,24 +1,24 @@
+# Inherit AOSP device configuration for d2vzw.
 $(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
-# Inherit some common aocp stuff.
+# Inherit common cdma apns
 $(call inherit-product, vendor/aocp/configs/cdma.mk)
 
-# Inherit some common aocp stuff.
+# Inherit common product files.
 $(call inherit-product, vendor/aocp/configs/common_phone.mk)
 
-# Camera
-PRODUCT_PACKAGES += \
-    Camera
-	
-#bootanimation
-PRODUCT_COPY_FILES += \
-        vendor/aocp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
+PRODUCT_PACKAGE_OVERLAYS += vendor/aocp/overlay/d2vzw
 
-#OTA
-PRODUCT_COPY_FILES += \
-        vendor/aocp/prebuilt/common/app/xhdpi/AoCPOTA.apk:system/app/AoCPOTA.apk
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.0.4/IMM76D/I535VRALG1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.0.4 IMM76D I535VRALG1 release-keys"
-
+# Setup device specific product configuration.
 PRODUCT_NAME := aocp_d2vzw
+PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := d2vzw
+PRODUCT_MODEL := SCH-I535
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.0.4/IMM76D/I535VRALG7:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.0.4 IMM76D I535VRALG7 release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+vendor/aocp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
+
